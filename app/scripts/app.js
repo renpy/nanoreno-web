92e67c5f85fecaacfb5df84d51f4652c046e13d6
Tom Rothamel <tom@rothamel.us>
2016-02-13 13:16:09 -0500

    Clean empty project.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,33 +3,27 @@
 /**
  * @ngdoc overview
  * @name nanorenoApp
- * @description
- * # nanorenoApp
+ * @description # nanorenoApp
  *
  * Main module of the application.
  */
-angular
-  .module('nanorenoApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module(
+		'nanorenoApp',
+		[ 'ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize',
+				'ngTouch' ]).config(
+		function($routeProvider, $locationProvider) {
+
+			$locationProvider.html5Mode(true);
+
+			$routeProvider.when('/', {
+				templateUrl : 'views/main.html',
+				controller : 'MainCtrl',
+				controllerAs : 'main'
+			}).when('/about', {
+				templateUrl : 'views/about.html',
+				controller : 'AboutCtrl',
+				controllerAs : 'about'
+			}).otherwise({
+				redirectTo : '/'
+			});
+		});
