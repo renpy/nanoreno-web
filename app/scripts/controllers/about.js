e92cc92576e59dcbedad8173533301fd2eeaf0c4
Tom Rothamel <tom@rothamel.us>
2016-02-13 00:01:03 -0500

    Initial checkin of yeoman site.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nanorenoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nanorenoApp
+ */
+angular.module('nanorenoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nanorenoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nanorenoApp
+ */
+angular.module('nanorenoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
